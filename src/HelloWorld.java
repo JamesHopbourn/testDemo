f6ac018fdfe927a0f75474c5895921eab74a69dc
JamesHopbourn <hopbourncontact@gmail.com>
2022-05-25 01:28:34 +0800

    delete 123
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("123");
         System.out.println("James");
         System.out.println("changed");
         System.out.println("444");
