e1424a251d8b19cb945e85cae7470e6dd5b3481f
JamesHopbourn <hopbourncontact@gmail.com>
2022-05-25 01:27:09 +0800

    delete one line
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,8 +4,5 @@ public class HelloWorld {
         System.out.println("James");
         System.out.println("changed");
         System.out.println("444");
-                System.out.println("666");
-                System.out.println("466");
-                System.out.println("446");
     }
 }
